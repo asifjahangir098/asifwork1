efd97e781e9bf3ddac53bd3855eeca5d8555075c
asifassg <asif.sh8634@gmail.com>
2025-05-09 14:53:57 -0700

    Add files via upload

---
+++ ASIF/q78main.dart
@@ -0,0 +1,26 @@
+void main() {
+  double farhenVal = 43;
+  double celsiusConvert = (farhenVal - 32) * 5 / 9;
+  print(celsiusConvert);
+  double celsiusVal = 43;
+  double farhenConvert = (celsiusVal * 9 / 5) + 32;
+  print(farhenVal);
+  double length = 5;
+  double breadth = 7;
+  double area = length * breadth;
+  print(area);
+
+  int num1 = 7;
+  int num2 = 8;
+  print(num1 + num2);
+  print((num1 + num2) / 3 + 5 * 5);
+  int english = 78;
+  int urdu = 56;
+  int match = 92;
+  print(english + urdu + match);
+  print((english + urdu + match) / 300 * 100);
+}
+
+
+
+
